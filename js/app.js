c121b078e383d5b0e8305ecd6aec1b4a44000173
François Kooman <fkooman@tuxed.net>
2013-08-13 11:46:14 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $.when($.ajax("log.json")).then(function(data, textStatus, jqXHR){
+        var source = $("#entry-template").html();
+        var template = Handlebars.compile(source);
+        
+        var idpLog = template({entities: data['saml20-idp']});
+        var spLog = template({entities: data['saml20-sp']});
+        $("#idpLog").html(idpLog);       
+        $("#spLog").html(spLog);
+    });
+});
