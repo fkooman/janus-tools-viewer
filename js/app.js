e97a6262c42c845aaef55fa2cc9d24e526e7c6c6
François Kooman <fkooman@tuxed.net>
2013-08-13 11:52:25 +0200

    format the JS

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
-    $.when($.ajax("log.json")).then(function(data, textStatus, jqXHR){
+    $.when($.ajax("log.json")).then(function (data, textStatus, jqXHR) {
         var source = $("#entry-template").html();
         var template = Handlebars.compile(source);
-        
-        var idpLog = template({entities: data['saml20-idp']});
-        var spLog = template({entities: data['saml20-sp']});
-        $("#idpLog").html(idpLog);       
+
+        var idpLog = template({
+            entities: data['saml20-idp']
+        });
+        var spLog = template({
+            entities: data['saml20-sp']
+        });
+        $("#idpLog").html(idpLog);
         $("#spLog").html(spLog);
     });
 });
